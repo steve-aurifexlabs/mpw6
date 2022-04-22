78cb65d6cb3f05fcfa325b9326ae3cc47cc5426c
Steve Goldsmith <steve@aurifexlabs.com>
2022-04-22 08:16:56 -0600

    Fix config.tcl

--- cpu/config.tcl
+++ cpu/config.tcl
@@ -22,7 +22,7 @@ set ::env(DESIGN_NAME) rvref
 
 set ::env(VERILOG_FILES) "\
 	$::env(CARAVEL_ROOT)/verilog/rtl/defines.v \
-	$script_dir/cpu/rtl.v"
+	$script_dir/rtl.v"
 
 set ::env(DESIGN_IS_CORE) 0
 
